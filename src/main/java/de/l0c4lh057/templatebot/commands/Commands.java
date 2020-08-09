b265ac41997f31fe85a543bbd8f2f1c1ae7fa2d7
Aaron <42084688+l0c4lh057@users.noreply.github.com>
2020-08-09 19:10:31 +0200

    switch to rest in help command

--- src/main/java/de/l0c4lh057/templatebot/commands/Commands.java
+++ src/main/java/de/l0c4lh057/templatebot/commands/Commands.java
@@ -5,10 +5,13 @@ import de.l0c4lh057.templatebot.main.BotMain;
 import de.l0c4lh057.templatebot.utils.BotUtils;
 import de.l0c4lh057.templatebot.utils.ratelimits.RatelimitType;
 import discord4j.common.GitProperties;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.entity.User;
+import discord4j.core.util.EntityUtil;
 import discord4j.discordjson.json.EmbedData;
 import discord4j.discordjson.json.EmbedFieldData;
+import discord4j.discordjson.json.MessageEditRequest;
 import io.github.bucket4j.Bandwidth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -43,52 +46,52 @@ public class Commands {
 		
 		Command.builder()
 				.setName("help")
-				.setRatelimit(RatelimitType.CHANNEL, Bandwidth.simple(3, Duration.ofSeconds(10)), Bandwidth.simple(7, Duration.ofHours(1)))
 				.setExecutor((event, language, prefix, args) -> {
-						if(!args.isEmpty()){
-							Command command = Commands.getCommand(args.get(0));
-							if(command != null){
-								String commandName = command.getName();
-								List<String> commandTree = new ArrayList<>();
-								commandTree.add(command.getName());
-								for(int i = 1; i < args.size(); i++){
-									Command cmd = command.getSubCommand(args.get(i));
-									if(cmd == null) break;
-									commandTree.add(cmd.getName());
-									command = cmd;
-								}
-								return event.getMessage().getRestChannel().createMessage(EmbedData.builder()
-										.title(getLanguageString(language, "help.command.title", commandName))
-										.description(getLanguageString(language, "help." + String.join(".", commandTree) + ".detailed", prefix))
-										.build()
-								);
+					if(!args.isEmpty()){
+						Command command = Commands.getCommand(args.get(0));
+						if(command != null){
+							String commandName = command.getName();
+							List<String> commandTree = new ArrayList<>();
+							commandTree.add(command.getName());
+							for(int i = 1; i < args.size(); i++){
+								Command cmd = command.getSubCommand(args.get(i));
+								if(cmd == null) break;
+								commandTree.add(cmd.getName());
+								command = cmd;
 							}
+							return event.getMessage().getRestChannel().createMessage(EmbedData.builder()
+									.title(getLanguageString(language, "help.command.title", commandName))
+									.description(getLanguageString(language, "help." + String.join(".", commandTree) + ".detailed", prefix))
+									.build()
+							);
 						}
-						Command.Category category = BotUtils.getHelpPage(language, args);
-						AtomicInteger helpPage = new AtomicInteger(category.getHelpPage());
-						// TODO switch from getChannel to getRestChannel
-						return event.getMessage().getChannel().flatMap(channel -> channel.createEmbed(BotUtils.getHelpSpec(language, prefix, category))
-								.flatMap(message -> Mono.when(
-										message.addReaction(BotUtils.EMOJI_ARROW_LEFT).then(message.addReaction(BotUtils.EMOJI_ARROW_RIGHT)),
-										event.getClient().on(ReactionAddEvent.class)
-												.filter(ev -> ev.getMessageId().equals(message.getId()))
-												.filter(ev -> ev.getUserId().equals(event.getMessage().getAuthor().map(User::getId).orElseThrow()))
-												.filter(ev -> ev.getEmoji().equals(BotUtils.EMOJI_ARROW_LEFT) || ev.getEmoji().equals(BotUtils.EMOJI_ARROW_RIGHT))
-												.timeout(Duration.ofMinutes(2), message.removeAllReactions().then(Mono.empty()))
-												.flatMap(ev -> {
-													if(ev.getEmoji().equals(BotUtils.EMOJI_ARROW_LEFT)) helpPage.decrementAndGet();
-													else if(ev.getEmoji().equals(BotUtils.EMOJI_ARROW_RIGHT)) helpPage.decrementAndGet();
-													helpPage.set(BotUtils.clamp(1, helpPage.get(), Command.Category.values().length));
-													Command.Category newCategory = Command.Category.getCategoryByHelpPage(helpPage.get());
-													return Mono.when(
-															message.removeReaction(ev.getEmoji(), ev.getUserId()),
-															message.edit(mes -> mes.setEmbed(BotUtils.getHelpSpec(language, prefix, Objects.requireNonNull(newCategory))))
-													);
-												})
-								))
-						);
 					}
-				)
+					Command.Category category = BotUtils.getHelpPage(language, args);
+					AtomicInteger helpPage = new AtomicInteger(category.getHelpPage());
+					return event.getMessage().getRestChannel().createMessage(BotUtils.getHelpEmbedData(language, prefix, category))
+							.flatMap(messageData -> Mono.when(
+									event.getMessage().getRestChannel().getRestMessage(Snowflake.of(messageData.id())).createReaction(EntityUtil.getEmojiString(BotUtils.EMOJI_ARROW_LEFT))
+											.then(event.getMessage().getRestChannel().getRestMessage(Snowflake.of(messageData.id())).createReaction(EntityUtil.getEmojiString(BotUtils.EMOJI_ARROW_RIGHT))),
+									event.getClient().on(ReactionAddEvent.class)
+											.filter(ev -> ev.getMessageId().asString().equals(messageData.id()))
+											.filter(ev -> ev.getUserId().equals(event.getMessage().getAuthor().map(User::getId).orElseThrow()))
+											.filter(ev -> ev.getEmoji().equals(BotUtils.EMOJI_ARROW_LEFT) || ev.getEmoji().equals(BotUtils.EMOJI_ARROW_RIGHT))
+											.timeout(Duration.ofMinutes(2), event.getMessage().getRestChannel().getRestMessage(Snowflake.of(messageData.id())).deleteAllReactions().then(Mono.empty()))
+											.flatMap(ev -> {
+												if(ev.getEmoji().equals(BotUtils.EMOJI_ARROW_LEFT)) helpPage.decrementAndGet();
+												else if(ev.getEmoji().equals(BotUtils.EMOJI_ARROW_RIGHT)) helpPage.incrementAndGet();
+												helpPage.set(BotUtils.clamp(1, helpPage.get(), Command.Category.values().length));
+												Command.Category newCategory = Command.Category.getCategoryByHelpPage(helpPage.get());
+												return Mono.when(
+														event.getMessage().getRestChannel().getRestMessage(Snowflake.of(messageData.id())).deleteUserReaction(EntityUtil.getEmojiString(ev.getEmoji()), ev.getUserId()),
+														event.getMessage().getRestChannel().getRestMessage(Snowflake.of(messageData.id())).edit(MessageEditRequest.builder()
+																.embed(BotUtils.getHelpEmbedData(language, prefix, Objects.requireNonNull(newCategory)))
+																.build()
+														)
+												);
+											})
+							));
+				})
 				.build().register();
 		
 		Command.collectionBuilder()
